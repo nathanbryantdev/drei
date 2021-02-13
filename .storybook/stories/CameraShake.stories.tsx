ad84aa6f99f48dfdc8021cd9f3c53b21cef3efc9
Gianmarco Simone <gsimone@hey.com>
2021-02-13 22:13:52 +0100

    minor shake story changes

--- .storybook/stories/CameraShake.stories.tsx
+++ .storybook/stories/CameraShake.stories.tsx
@@ -4,14 +4,14 @@ import { useFrame } from 'react-three-fiber'
 
 import { Setup } from '../Setup'
 
-import { CameraShake, ShakeConfigPartial, OrbitControls } from '../../src'
+import { CameraShake, ShakeConfigPartial, OrbitControls, Box, Plane } from '../../src'
 
 export default {
   title: 'Camera/CameraShake',
   component: CameraShake,
   decorators: [
     (storyFn) => (
-      <Setup cameraPosition={new THREE.Vector3(0, 0, 10)} controls={false}>
+      <Setup cameraPosition={new THREE.Vector3(0, 3, 10)} controls={false}>
         {storyFn()}
       </Setup>
     ),
@@ -29,14 +29,11 @@ function Scene() {
 
   return (
     <>
-      <mesh ref={cube}>
-        <boxBufferGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial wireframe color="white" />
-      </mesh>
-      <mesh position={[0, -6, 0]} rotation={[Math.PI / -2, 0, 0]}>
-        <planeBufferGeometry args={[200, 200, 75, 75]} />
-        <meshBasicMaterial wireframe color="red" side={THREE.DoubleSide} />
-      </mesh>
+      <Box position={[6, 2.5, -10]} args={[1, 5, 1]} material-color="hotpink" />
+      <Box position={[-6, 4, -10]} args={[1, 8, 1]} material-color="hotpink" />
+
+      <Box ref={cube} position-y={2} args={[2, 2, 2]} material-color="hotpink" />
+      <gridHelper args={[40, 40, 40]} />
     </>
   )
 }
@@ -67,9 +64,9 @@ function CameraShakeWithOrbitScene({ cfg }) {
 }
 
 const controlsConfig: ShakeConfigPartial = {
-  maxYaw: 0.05,
-  maxPitch: 0.05,
-  maxRoll: 0.05,
+  maxYaw: 0.025,
+  maxPitch: 0.025,
+  maxRoll: 0.025,
   yawFrequency: 0.8,
   pitchFrequency: 0.8,
   rollFrequency: 0.8,
